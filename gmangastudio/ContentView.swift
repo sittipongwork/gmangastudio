21c9bee89e927c705cee800ed41942fa4ecbdc4e
Sittipong Jangsakun <sittipongwork@gmail.com>
2026-07-09 13:19:25 +0700

    Initial Commit

---
+++ gmangastudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gmangastudio
+//
+//  Created by Sittipong Jungsakul on 9/7/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gmangastudio/gmangastudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  gmangastudioApp.swift
+//  gmangastudio
+//
+//  Created by Sittipong Jungsakul on 9/7/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct gmangastudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
